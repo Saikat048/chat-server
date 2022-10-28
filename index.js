26ab5943ea7427a1cc55cb31a9b41726c7c96371
Saikat048 <mdsaikathosan048@gmail.com>
2022-10-28 15:53:28 +0600

    first commit

--- index.js
+++ index.js
@@ -7,7 +7,35 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-
-server.listen(3001, () => {
-  console.log("SERVER RUNNING");
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.broadcast.emit('message', 'A user has joined the chat')
+
+  socket.emit("message", 'Welcome to ChatCord')
+  socket.on("joinRoom", room => {
+		socket.join(room)
+  })
+
+  socket.on("newMessage", ({newMessage, room}) => {
+    io.in(room).emit("getLatestMessage", newMessage)
+  })
+ 
+ 
+});
+
+
+
+server.listen(5000, () => {
+  console.log("SERVER RUNNING");
+}); 
+ 
+  
+ 
